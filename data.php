b3ddd419db8d3dbb47f06493c78ba4a699dbd74b
febrimanalu <54320024+febrimanalu@users.noreply.github.com>
2021-04-07 22:10:07 +0700

    perbaikan sesuai tampilan
    index : done
    data : nope
    grafik : perbaiki warna
    prediksi : function blom

--- data.php
+++ data.php
@@ -1,25 +1,24 @@
 <?php
 include("koneksi.php");
 include("HeaderFooter/header.php");
-
 ?>
-        <main role="menu">
-            <div class="container">
-            <br>
-            <form action="" method="POST" enctype="multipart/form-data">
-                <input type="file" name="file">
-                <input type="submit" name="upload" value="Upload">
-            </form>
-            </div>
-        </main>
-    <!--Javascript-->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
-    </body>
-</html>
+
+<main role="menu">
+    <div class="container">
+        <br>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" name="upload" value="Upload">
+        </form>
+    </div>
+</main>
+
+<!--Javascript-->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php
 include("koneksi.php");
 include("HeaderFooter/header.php");
-
 ?>
-        <main role="menu">
-        <center>
-            <div class="card border-primary"> 
-                <div class="row">
-                    <div class="col-md-5">
+
+<style>
+    .card {
+        width: 1100px;
+        height: 350px;
+        margin-top: 125px;
+        margin-left: 30px;
+        margin-right:30px;
+    }
+</style>
+
+<main role="menu">
+    <center>
+        <div class="card border-primary"> 
+            <div class="row">
+                <div class="col-md-5">
                     <img src="img/Keysight2.png" alt="ict1">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body text-dark">
-                            <br>
-                            <h4 class="card-title text-center">In Circuit Testing Machine</h4>
-                            <br>
-                            <p class="card-text">
-                            Mesin ICT (In Circuit Testing) adalah sebuah mesin yang berfungsi untuk melakukan uji papan
-                            sirkuit cetak. Parameter seperti resistensi, kapasitance dan sebagainya semuanya diukur bersama
-                            dengan pengoperasian komponen analog seperti penguat operasional. Maka mesin ICT, dimungkinkan untuk 
-                            melakukan bentuk tes papan sirkuit cetak yang sangat komprehensif, memastikan bahwa sirkuit telah 
-                            diproduksi dengan benar dan memiliki peluang yang sangat tinggi untuk tampil sesuai spesifikasinya.
-                            </p>
-                        </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body text-dark">
+                        <br>
+                        <h4 class="card-title text-center">In Circuit Testing Machine</h4>
+                        <br>
+                        <p class="card-text">
+                        Mesin ICT (In Circuit Testing) adalah sebuah mesin yang berfungsi untuk melakukan uji papan
+                        sirkuit cetak. Parameter seperti resistensi, kapasitance dan sebagainya semuanya diukur bersama
+                        dengan pengoperasian komponen analog seperti penguat operasional. Maka mesin ICT, dimungkinkan untuk 
+                        melakukan bentuk tes papan sirkuit cetak yang sangat komprehensif, memastikan bahwa sirkuit telah 
+                        diproduksi dengan benar dan memiliki peluang yang sangat tinggi untuk tampil sesuai spesifikasinya.
+                        </p>
                     </div>
                 </div>
             </div>
-        </center>
+        </div>
+    </center>
         
-        <!--Javascript-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script>
-            $('.carousel').carousel({
-                interval: 2000
-            })
-        </script>
-        </main>
-    </body>
-</html>
+<!--Javascript-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</main>
