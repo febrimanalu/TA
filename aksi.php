7eb3ec6d15d32e17c0363db52dd6ad90bcee8c75
febrimanalu <54320024+febrimanalu@users.noreply.github.com>
2021-06-08 15:47:51 +0700

    DONE PREDIKSI

--- aksi.php
+++ aksi.php
@@ -7,47 +7,47 @@ if(isset($_POST['submit'])){
 //echo "$def";
 //echo "$sym";
 //echo "$rel";
-    if(($def == "Analog Intermitent") && ($sym == "PreShort Fail") && ($rel == "CARD")){
-        $pred == "Repair";
+    if(($def = "Analog Intermitent") && ($sym = "PreShort Fail") && ($rel = "CARD")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Corrupted") && ($sym == "BT-Basic Error") && ($rel == "PC CONTROLLER")){
-        $pred == "Reboot";
+    } else if(($def = "Corrupted") && ($sym = "BT-Basic Error") && ($rel = "PC CONTROLLER")){
+        $pred = "Reboot";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Corrupted") && ($sym == "No Booting") && ($rel == "PC CONTROLLER")){
-        $pred == "Restore";
+    } else if(($def = "Corrupted") && ($sym = "No Booting") && ($rel = "PC CONTROLLER")){
+        $pred = "Restore";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Fail Tesjet") && ($sym == "Ground") && ($rel == "CARD")){
-        $pred == "Repair";
+    } else if(($def = "Fail Tesjet") && ($sym = "Ground") && ($rel = "CARD")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "No Booting") && ($sym == "No Booting") && ($rel == "PC CONTROLLER")){
-        $pred == "Restore";
+    } else if(($def = "No Booting") && ($sym = "No Booting") && ($rel = "PC CONTROLLER")){
+        $pred = "Restore";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "No Booting") && ($sym == "Tester Short") && ($rel == "OTHER")){
-        $pred == "Repair";
+    } else if(($def = "No Booting") && ($sym = "Tester Short") && ($rel = "OTHER")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Overvoltage") && ($sym == "Overvoltage") && ($rel == "MPU")){
-        $pred == "Reboot";
+    } else if(($def = "Overvoltage") && ($sym = "Overvoltage") && ($rel = "MPU")){
+        $pred = "Reboot";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Overvoltage") && ($sym == "Power Supply Problem") && ($rel == "OTHER")){
-        $pred == "Repair";
+    } else if(($def = "Overvoltage") && ($sym = "Power Supply Problem") && ($rel = "OTHER")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "PC Problem") && ($sym == "PC Blue Screen") && ($rel == "PC CONTROLLER")){
-        $pred == "Restore";
+    } else if(($def = "PC Problem") && ($sym = "PC Blue Screen") && ($rel = "PC CONTROLLER")){
+        $pred = "Restore";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "PC Problem") && ($sym == "PC Blue Screen") && ($rel == "OTHER")){
-        $pred == "Repair";
+    } else if(($def = "PC Problem") && ($sym = "PC Blue Screen") && ($rel = "OTHER")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Short Card") && ($sym == "Tester Short") && ($rel == "DIAGNOSTIC")){
-        $pred == "Repair";
+    } else if(($def = "Short Card") && ($sym = "Tester Short") && ($rel = "DIAGNOSTIC")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Short Card") && ($sym == "Tester Short") && ($rel == "CARD")){
-        $pred == "Repair";
+    } else if(($def = "Short Card") && ($sym = "Tester Short") && ($rel = "CARD")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Vaccum Issue") && ($sym == "Problem Vaccum") && ($rel == "OTHER")){
-        $pred == "False Call";
+    } else if(($def = "Vaccum Issue") && ($sym = "Problem Vaccum") && ($rel = "OTHER")){
+        $pred = "False Call";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
-    } else if(($def == "Vaccum Issue") && ($sym == "Problem Vaccum") && ($rel == "AIR PRESSURE")){
-        $pred == "Repair";
+    } else if(($def = "Vaccum Issue") && ($sym = "Problem Vaccum") && ($rel = "AIR PRESSURE")){
+        $pred = "Repair";
         header("location: prediksi1.php?def=$def&sym=$sym&rel=$rel&pred=$pred");
     }
 }
